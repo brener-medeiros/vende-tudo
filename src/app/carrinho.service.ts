d668371e27eb7b338c7f348a954f97ae0cea44f4
Brener Medeiros <brenermedeiros@gmail.com>
2023-01-21 23:33:21 -0300

    build

---
+++ src/app/carrinho.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { IProdutoCarrinho } from './produtos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarrinhoService {
+  itens: IProdutoCarrinho[] = [];
+
+  constructor() { }
+
+  obtemCarrinho() {
+    this.itens = JSON.parse(localStorage.getItem("carrinho") || "[]");
+    return this.itens;
+  }
+  adicionarAoCarrinho(produto: IProdutoCarrinho) {
+    this.itens.push(produto);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens))
+  }
+  removerPrudutoCarrinho(produtoId: number) {
+    this.itens = this.itens.filter(item => item.id !== produtoId);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens));
+  }
+  limpaCarrinho() {
+    this.itens = [];
+    localStorage.clear();
+  }
+
+
+}
